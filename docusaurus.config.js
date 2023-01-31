95b6b851b4d2d81cbcfa0f92743c796cc3afd2bd
Jihee <jihee@deepbrainai.io>
2023-01-31 17:48:16 +0900

    update config url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'DeepBrain AI DOCS',
   tagline: 'Welcome! Here you can read the documentation for the AI Human.',
-  url: 'https://deepbrainai.io/',
+  url: 'https://docs.deepbrainai.io/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
